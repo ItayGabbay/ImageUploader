2fc8d1f03ca2fbf6ce119e9af979acbbde83ffc6
ItayGabbay <itay240594@gmail.com>
2018-05-30 20:40:55 +0300

    Adding prediction URL

--- public/js/search.js
+++ public/js/search.js
@@ -1,6 +1,6 @@
 
 // Put your prediction endpoint here
-const PREDICTIONS_URL = "None"
+const PREDICTIONS_URL = "http://image-det-route-image-detector.52.166.94.187.nip.io/image/"
 
 
 var search = function() {
@@ -13,7 +13,7 @@ var search = function() {
         $('#searchResults').text(data)
     })
     .fail(function(err) {
-        $('#searchResults').text("Error while searching for image predictions. Maybe You forgot to change the prediction URL? ")
+        $('#searchResults').text("Error while searching for image predictions. Maybe You forgot to change the prediction URL? ;)")
     })
 
 }
